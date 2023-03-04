d0d435091622c03ba495192c8263d53ac9a0bef4
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-03-03 21:03:26 -0500

    Debian update

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# WI-FI Tool
+# WI-FI Tool #<b>This tool is in maintenance</b>
 
 WI-FI Deauthentication Attack
+
+## Requirements
+
+### [Aircrack-ng](https://www.aircrack-ng.org/doku.php?id=install_aircrack#installing_aircrack-ng_from_source)
+
+``` bash
+# Aircrack-ng requirements
+sudo apt-get update
+sudo apt-get install autoconf automake libtool shtool pkg-config libssl-dev ethtool rfkill libnl-3-dev libnl-genl-3-dev build-essential libstdc++-11-dev
+# Aircrack-ng
+wget https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz && tar -zxvf aircrack-ng-1.7.tar.gz
+cd aircrack-ng-1.7
+autoreconf -i
+./configure --with-experimental
+make
+make install
+ldconfig
+```
+
 ## main.sh
 
 This script setup our environment
--- main.sh
+++ main.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+                                                                                                  
+function dependencies(){
+	clear; dependencies=(aircrack-ng macchanger)
+
+	echo -e "${yellowColour}[*]${endColour}${grayColour} Checking programs...${endColour}"
+	sleep 2
+
+	for program in "${dependencies[@]}"; do
+		echo -ne "\n${yellowColour}[*]${endColour}${blueColour} Tools ${endColour}${purpleColour} $program${endColour}${blueColour}...${endColour}"
+
+		test -f /usr/bin/$program
+
+		if [ "$(echo $?)" == "0" ]; then
+			echo -e " ${greenColour}(V)${endColour}"
+		else
+			echo -e " ${redColour}(X)${endColour}\n"
+			echo -e "${yellowColour}[*]${endColour}${grayColour} Installing ${endColour}${blueColour}$program${endColour}${yellowColour}...${endColour}"
+			apt-get install $program -y > /dev/null 2>&1
+		fi; sleep 1
+	done
+}
+
 if [ "$(id -u)" == "0" ]; then
   clear
   # Mac Address
